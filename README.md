faa21a4ada80acb373f1cbcd55dcc1418a97a14a
NewLuminous <newluminous7@gmail.com>
2020-01-28 20:17:14 +0700

    Init Django, update README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # UTDVN
 Search engine service backed by Apache Solr, Django and Scrapy for undergraduate theses and dissertations
+
+### Useful Links
+[Installing Python 3.8](https://www.python.org/downloads/)
+[Getting Started with Django](https://www.djangoproject.com/start/)
---
+++ UTDVN_backend/UTDVN_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for UTDVN_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UTDVN_backend.settings')
+
+application = get_asgi_application()
